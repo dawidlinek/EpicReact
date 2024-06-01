91e990d8df0213f6966e20a972df632fff05f15d
Dawid Linek <dawidlinek@op.pl>
2024-06-01 21:46:36 +0200

    feat: styling base

--- react-fundamentals/src/exercise/05.js
+++ react-fundamentals/src/exercise/05.js
@@ -13,10 +13,12 @@ import '../box-styles.css'
 // matches what the text says it should be
 // 🐨 also use the style prop to make the font italic
 // 💰 Here are available style attributes: backgroundColor, fontStyle
-
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+const boxStyles = {
+  fontStyle: 'italic'
+}
+const smallBox = <div className="box box--small" style={{ backgroundColor: 'lightblue', ...boxStyles }}>small lightblue box</div>
+const mediumBox = <div className="box box--medium" style={{ backgroundColor: 'pink', ...boxStyles }}>medium pink box</div>
+const largeBox = <div className="box box--large" style={{ backgroundColor: 'orange', ...boxStyles }}>large orange box</div>
 
 function App() {
   return (

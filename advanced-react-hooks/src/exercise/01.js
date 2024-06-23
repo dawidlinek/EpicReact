a6f6b9ef64d079d69226af94d4031e1250b69571
Dawid Linek <dawid@linek.dev>
2024-06-23 14:21:12 +0200

    feat: useReducer: simple Counter extra 3

--- advanced-react-hooks/src/exercise/01.js
+++ advanced-react-hooks/src/exercise/01.js
@@ -5,16 +5,19 @@ import * as React from 'react'
 
 function Counter({ initialCount = 0, step = 1 }) {
   function countReducer(state, newState) {
-    return {...state, ...newState}
+    return {
+      ...state,
+      ...(newState instanceof Function ? newState(state) : newState)
+    }
   }
   // 🐨 replace React.useState with React.useReducer.
   // 💰 React.useReducer(countReducer, initialCount)
   const [state, setState] = React.useReducer(countReducer, {
     count: initialCount,
   })
-  const {count} = state
-  const increment = () => setState({count: count + step})
-  
+  const { count } = state
+  const increment = () => setState(currentState => ({ count: currentState.count + step }))
+
   return <button onClick={increment}>{count}</button>
 }
 
